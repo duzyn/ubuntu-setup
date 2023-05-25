5505d7fae005ec98d71f7c895f60a748df998a0d
David Peng <davidduzyn@outlook.com>
2023-05-25 23:07:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -365,8 +365,10 @@ npm update -g
 # https://mirrors.ustc.edu.cn/CTAN/systems/win32/miktex/doc/miktex.pdf
 if ! dpkg -s "miktex" &>/dev/null; then
     log "Installing MiKTeX..."
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
-    echo "deb http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 \
+        --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+    echo "deb http://miktex.org/download/ubuntu focal universe" | \
+        sudo tee /etc/apt/sources.list.d/miktex.list
     sudo apt-get update
     sudo apt-get install -y miktex
 else
