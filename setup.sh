4133f75ccac6bd4246bc64c545b6e3008e08867b
David Peng <duzyn@users.noreply.github.com>
2023-07-05 02:45:45 +0800

    更新 setup.sh

--- setup.sh
+++ setup.sh
@@ -222,6 +222,7 @@ command -v code &>/dev/null || {
 command -v latex &>/dev/null || {
     wget -P "$TEMP_DIR" "$CTAN_MIRROR/systems/texlive/tlnet/install-tl-unx.tar.gz"
     tar --extract --gz --directory "$TEMP_DIR" --file "$TEMP_DIR/install-tl-unx.tar.gz"
+    tree "$TEMP_DIR"
     cd "$TEMP_DIR/install-tl-unx" || exit 1
     perl ./install-tl --no-interaction --scheme=basic --no-doc-install --no-src-install --location "$CTAN_MIRROR/systems/texlive/tlnet"
     #  PATH=/usr/local/texlive/2023/bin/x86_64-linux:$PATH 
